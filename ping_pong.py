45e5fa2bc5ab7fa5ad857e5d2870dede59fecb13
HornsAndHoovesCompany <99079012+HornsAndHoovesCompany@users.noreply.github.com>
2022-02-12 17:02:54 +0400

    Update ping_pong.py
    v0.2-added class Gamesprite

--- ping_pong.py
+++ ping_pong.py
@@ -1,6 +1,17 @@
 from pygame import *
 from random import randint
-#100500 strings of code later...
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image, player_x, player_y, size_x, size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image), (size_x, size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+        self.direction = None
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
 clock = time.Clock()
 FPS = 60
 back = (50, 140, 42)
